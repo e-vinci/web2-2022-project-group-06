c5c9ab32c857aa27ff5aaa84b14f9143671f3314
Brandon <brandon.vanbellinghen@student.vinci.be>
2022-12-12 10:38:34 +0100

    roulette suite

--- frontend/src/Components/Pages/RoulettePage.js
+++ frontend/src/Components/Pages/RoulettePage.js
@@ -4,56 +4,124 @@ import jQuery from 'jquery';
 const RoulettePage = () => {
   const main = document.querySelector('main');
     const roulette = ` 
-    <div class='roulette-wrapper'>
-  <div class='selector'></div>
-  <div class='wheel'></div>
-</div>
-
-<div>
-  <input placeholder='outcome'>
-  <button>
-    Spin Wheel
-  </button>
-</div>
+    <div id="roulette">
+      <div class="case">
+        <div class="green">0</div>
+      </div>
+      <div class="case">
+        <div class="red">1</div>
+      </div>
+      <div class="case">
+        <div class="black">2</div>
+      </div>
+       <div class="case">
+        <div class="red">3</div>
+      </div>
+      <div class="case">
+        <div class="black">4</div>
+       </div>
+  <div class="case">
+  <div class="red">5</div>
+  </div>
+  <div class="case">
+  <div class="black">6</div>
+  </div>
+  <div class="case">
+  <div class="red">7</div>
+  </div>
+  <div class="case">
+  <div class="black">8</div>
+  </div>
+  <div class="case">
+  <div class="red">9</div>
+  </div>
+  <div class="case">
+  <div class="black">10</div>
+  </div>
+  <div class="case">
+  <div class="red">11</div>
+  </div>
+  <div class="case">
+  <div class="black">12</div>
+  </div>
+  <div class="case">
+  <div class="red">13</div>
+  </div>
+  <div class="case">
+  <div class="black">14</div>
+  </div>
+  <div class="case">
+  <div class="red">15</div>
+  </div>
+  <div class="case">
+  <div class="black">16</div>
+  </div>
+  <div class="case">
+  <div class="red">17</div>
+  </div>
+  <div class="case">
+  <div class="black">18</div>
+  </div>
+  <div class="case">
+  <div class="red">19</div>
+  </div>
+  <div class="case">
+  <div class="black">20</div>
+  </div>
+  <div class="case">
+  <div class="red">21</div>
+  </div>
+  <div class="case">
+  <div class="black">22</div>
+  </div>
+  <div class="case">
+  <div class="red">23</div>
+  </div>
+  <div class="case">
+  <div class="black">24</div>
+  </div>
+  <div class="case">
+  <div class="red">25</div>
+  </div>
+  <div class="case">
+  <div class="black">26</div>
+  </div>
+  <div class="case">
+  <div class="red">27</div>
+  </div>
+  <div class="case">
+  <div class="black">28</div>
+  </div>
+  <div class="case">
+  <div class="red">29</div>
+  </div>
+  <div class="case">
+  <div class="black">30</div>
+  </div>
+  <div class="case">
+  <div class="red">31</div>
+  </div>
+  <div class="case">
+  <div class="black">32</div>
+  </div>
+  <div class="case">
+  <div class="red">33</div>
+  </div>
+  <div class="case">
+  <div class="black">34</div>
+  </div>
+  <div class="case">
+  <div class="red">35</div>
+  </div>
+  <div class="case">
+  <div class="black">36</div>
+  </div>
+  <div class="case">
+  <div class="red">37</div>
+  </div>
+    </div>
     `;
     main.innerHTML = roulette;
 };
-
-$(document).ready(function() {
-	//setup multiple rows of colours, can also add and remove while spinning but overall this is easier.
-	initWheel();
- 
- 	$('button').on('click', function(){
-		var outcome = parseInt($('input').val());
-    spinWheel(outcome);
-  });
-});
-
-function initWheel(){
-	var $wheel = $('.roulette-wrapper .wheel'),
-  		row = "";
-      
-  row += "<div class='row'>";
-  row += "  <div class='card red'>1</div>";
-  row += "  <div class='card black'>14</div>";
-  row += "  <div class='card red'>2</div>";
-  row += "  <div class='card black'>13</div>";
-  row += "  <div class='card red'>3</div>";
-  row += "  <div class='card black'>12</div>";
-  row += "  <div class='card red'>4</div>";
-  row += "  <div class='card green'>0</div>";
-  row += "  <div class='card black'>11</div>";
-  row += "  <div class='card red'>5</div>";
-  row += "  <div class='card black'>10</div>";
-  row += "  <div class='card red'>6</div>";
-  row += "  <div class='card black'>9</div>";
-  row += "  <div class='card red'>7</div>";
-  row += "  <div class='card black'>8</div>";
-  row += "</div>";
-  
-	for(var x = 0; x < 29; x++){
-  	$wheel.append(row);
-  }
-}
 
 export default RoulettePage;
