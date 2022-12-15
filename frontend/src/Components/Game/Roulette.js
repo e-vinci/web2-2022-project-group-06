a60906fc9e2213b10b5232a38288ae0e8e8dc555
Brandon <brandon.vanbellinghen@student.vinci.be>
2022-12-15 12:28:29 +0100

    reglage roulette

--- frontend/src/Components/Game/Roulette.js
+++ frontend/src/Components/Game/Roulette.js
@@ -6,7 +6,7 @@ let nums = [];
 let bets = [];
 let h = 1;
 
-function history() {
+function historyRoll() {
   let i;
   for (i = 0; i < h; i++) {
     document.getElementById("a" + i).style = "background: " + cols[i] + ";";
@@ -25,30 +25,27 @@ function history() {
   }
 }
 
+
 function roll(color) {
 
-  let a = bal;
-  let winc = 0;
-  let bet = document.getElementById("bet").value;
+  var a = bal;
+  var winc = 0;
+  var bet = document.getElementById("bet").value;
   bet = parseInt(bet);
-  if(bet < 0){
-    document.getElementById("msg").innerHTML = "Bet too low!";
-    return;
-  }
   if (bet > bal) {
     document.getElementById("msg").innerHTML = "Bet too high!";
   } else {
-    let win = Math.floor((Math.random() * 29) + 0);
+    var win = Math.floor((Math.random() * 29) + 0);
     if (win > 4) {
-      let rollm = win * 40 - 40 * 4.2;
+      var rollm = win * 40 - 40 * 4.2;
       document.getElementById("roll").style = "margin-left: -" + rollm + "px ";
     }
     if (win < 4) {
-      let rollm = 180 - 40 * win -20;
+      var rollm = 180 - 40 * win -20;
       document.getElementById("roll").style = "margin-left: " + rollm + "px ";
     }
     if (win == 4) {
-      let rollm = 0;
+      var rollm = 0;
       document.getElementById("roll").style = "margin-left: -" + rollm + "px ";
     }
     if (win % 2 != 2) {
@@ -68,16 +65,17 @@ function roll(color) {
         bal = bal + bet * 14;
         bets.push("<br><font style='color: green;'>+"+bet*14+"</font>");
       }
+    } else {
       bal -= bet;
       bets.push("<br><font style='color: red;'>-"+bet+"</font>");
-  }
+    }
     cols.push(winc);
     nums.push(win);
-    history();
+    historyRoll();
     document.getElementById("bal").innerHTML = bal;
     document.getElementById("stat").innerHTML = bets;
   }
 }
 
-export {roll,history,bal};
+export {roll,historyRoll,bal};
 
