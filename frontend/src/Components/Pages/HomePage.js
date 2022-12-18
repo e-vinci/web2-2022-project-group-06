8e6e2f4d74a65d4dcc3c4cfe2c47e5178cb3eb1b
Brandon <brandon.vanbellinghen@student.vinci.be>
2022-12-18 21:22:56 +0100

    changement lien menu carrousel

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -12,22 +12,22 @@ const Homepage = () => {
   <div class="container">
   <div class="slider">
     <div class="box1">
-    <a class="nav-link" aria-current="page" href="/blackJack" data-uri="/blackJack"><img class="ImageMenu" src="${image1}"></a>
+    <a class="nav-link" aria-current="page" href="#" data-uri="/blackJack"><img class="ImageMenu" src="${image1}"></a>
     </div>
     <div class="box2">
-    <a class="nav-link" aria-current="page" href="/slotMachine" data-uri="/slotMachine"><img class="ImageMenu" src="${image2}"></a>
+    <a class="nav-link" aria-current="page" href="#" data-uri="/slotMachine"><img class="ImageMenu" src="${image2}"></a>
     </div>
     <div class="box3">
-    <a class="nav-link" aria-current="page" href="/roulette" data-uri="/roulette"><img class="ImageMenu" src="${image3}"></a>
+    <a class="nav-link" aria-current="page" href="#" data-uri="/roulette"><img class="ImageMenu" src="${image3}"></a>
     </div>
     <div class="box4">
-    <a class="nav-link" aria-current="page" href="/blackJack" data-uri="/blackJack"><img class="ImageMenu" src="${image1}"></a>
+    <a class="nav-link" aria-current="page" href="#" data-uri="/blackJack"><img class="ImageMenu" src="${image1}"></a>
     </div>
     <div class="box5">
-    <a class="nav-link" aria-current="page" href="/slotMachine" data-uri="/slotMachine"><img class="ImageMenu" src="${image2}"></a>
+    <a class="nav-link" aria-current="page" href="#" data-uri="/slotMachine"><img class="ImageMenu" src="${image2}"></a>
     </div>
     <div class="box6">
-    <a class="nav-link" aria-current="page" href="/roulette" data-uri="/roulette"><img class="ImageMenu" src="${image3}"></a>
+    <a class="nav-link" aria-current="page" href="#" data-uri="/roulette"><img class="ImageMenu" src="${image3}"></a>
     </div>
   </div>
 </div>
